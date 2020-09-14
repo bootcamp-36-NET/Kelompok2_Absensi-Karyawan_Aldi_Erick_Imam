200725920f91f79d63b5ec24cd9eba3974f04447
iman <imntama@gmail.com>
2020-09-14 15:08:08 +0700

    Update Employee

--- API/Context/MyContext.cs
+++ API/Context/MyContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +9,13 @@ namespace API.Context
 {
     public class MyContext : DbContext
     {
-        public MyContext(DbContextOptions<MyContext> options) : base(options)
-        {
-
-        }
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
 
 
 
 
-        
+
+
     }
 }
