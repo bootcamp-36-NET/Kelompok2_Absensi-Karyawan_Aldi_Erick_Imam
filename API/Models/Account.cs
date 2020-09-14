9d0479ef8d87c9cc7ec3fa637aba564856cb0e02
erick-alex-sander <55838569+erick-alex-sander@users.noreply.github.com>
2020-09-14 14:48:02 +0700

    Changes 1

---
+++ API/Models/Account.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    [Table("tb_m_user")]
+    public class User : IdentityUser
+    {
+        public ICollection<UserRole> UserRoles { get; set; }
+    }
+
+    [Table("tb_m_role")]
+    public class Role : IdentityRole
+    {
+        public ICollection<UserRole> UserRoles { get; set; }
+    }
+
+    [Table("tb_m_userrole")]
+    public class UserRole : IdentityUserRole<string>
+    {
+        [ForeignKey("User")]
+        public override string UserId { get; set; }
+
+        [ForeignKey("Role")]
+        public override string RoleId { get; set; }
+    }
+}
