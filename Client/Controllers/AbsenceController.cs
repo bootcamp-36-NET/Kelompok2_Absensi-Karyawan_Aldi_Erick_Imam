d3d839f8390903664ae644b7bfca0a634c3482cb
erick-alex-sander <55838569+erick-alex-sander@users.noreply.github.com>
2020-09-17 20:53:42 +0700

    Absence Page

---
+++ Client/Controllers/AbsenceController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Client.Controllers
+{
+    public class AbsenceController : Controller
+    {
+        HttpClient client = new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:44381/api/")
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/Absence")]
+        public async Task<IActionResult> SendAbsence(string userName)
+        {
+            var content = JsonConvert.SerializeObject(userName);
+
+            var postTask = await client.PostAsync("Absence", new StringContent(content, Encoding.UTF8, "application/json"));
+
+            if (postTask.IsSuccessStatusCode)
+            {
+                return View();
+            }
+            return View();
+        }
+    }
+}
--- Client/Controllers/AccountsController.cs
+++ Client/Controllers/AccountsController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using Client.ViewModels;
+using API.ViewModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -27,7 +27,7 @@ namespace Client.Controllers
 
         [HttpPost]
         [Route("/Login")]
-        public async Task<IActionResult> Login(LoginVM login)
+        public async Task<IActionResult> Login(LoginVm login)
         {
             var content = JsonConvert.SerializeObject(login);
 
